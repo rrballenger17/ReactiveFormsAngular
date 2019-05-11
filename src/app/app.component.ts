e8cf05a0369bcfec65ae65b7ae709c9dbce7b0fd
Ryan Ballenger <ryan@Ryans-MacBook-Pro.local>
2019-05-10 22:21:03 -0400

    complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ReactiveFormsAngular';
+
+  myFormModel: FormGroup;                    
+
+  constructor() {
+    this.myFormModel = new FormGroup({       
+       username: new FormControl(),
+        fullName: new FormControl(),
+      
+        passwordsGroup: new FormGroup({        
+           password: new FormControl(),
+            pconfirm: new FormControl()
+        }),
+      emails: new FormArray([                  
+        new FormControl()                      
+      ])
+
+    });
+  }
+
+  addEmail() {
+    this.emailsFormArray.push(new FormControl());
+  
+  }
+
+
+  get emailsFormArray() {
+    return this.myFormModel.get('emails') as FormArray;
+  }
+
+
+  onSubmit() {
+    console.log(this.myFormModel.value);     
+   }
+
+
 }
+
+
+
+
